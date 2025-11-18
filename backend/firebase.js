1c7d7b18124a58e4cbcd5988b36458bf282c4a22
LazyBeaver007 <adiprime007@gmail.com>
2025-11-18 19:57:23 +0530

    Added firebase config

---
+++ backend/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AssIzaSyAmL6ujzvuqvP3PozdVZfoKd475IQ-tvV4-pesudo-api",
+  authDomain: "gitmatch-20e6d.firebaseapp.com",
+  projectId: "gitmatch-20e6d",
+  storageBucket: "gitmatch-20e6d.firebasestorage.app",
+  messagingSenderId: "1021880418142",
+  appId: "1:1021880418142:web:f8dc7866352a986ae0d9cf",
+  measurementId: "G-WG0425K2NK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
