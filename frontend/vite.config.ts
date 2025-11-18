59665b4151c73e6515cfa70253b308f94b264bf7
NYC! <neerajyamaji@gmail.com>
2025-11-18 18:48:55 +0530

    installed and completed setup of tailwind css

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000'
-    }
-  }
-})
+	plugins: [react(), tailwindcss()],
+	server: {
+		proxy: {
+			"/api": "http://localhost:3000",
+		},
+	},
+});
