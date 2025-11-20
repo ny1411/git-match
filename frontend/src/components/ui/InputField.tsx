8c2bd98e5391cf15127b4d205b52b7ed7812bb56
NYC! <neerajyamaji@gmail.com>
2025-11-20 11:59:06 +0530

    fix: improve formatting and styling in InputField component

--- frontend/src/components/ui/InputField.tsx
+++ frontend/src/components/ui/InputField.tsx
@@ -1,16 +1,25 @@
 import type { FC, InputHTMLAttributes } from "react";
 
 interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
-    label: string;
+	label: string;
 }
-export const InputField: FC<InputFieldProps> = ({ label, ...props}) => (
+export const InputField: FC<InputFieldProps> = ({ label, ...props }) => (
 	<div className="group relative z-0 w-full mb-8">
 		<input
 			{...props}
 			placeholder=" "
-			className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-purple-500 peer"
+			className={`block py-2.5 px-0 w-full text-sm text-white bg-transparent 
+			border-0 border-b-2 border-gray-300 appearance-none 
+			focus:outline-none focus:ring-0 focus:border-purple-500 peer ${
+				props.disabled ? "cursor-not-allowed" : ""
+			}`}
+			title={props.disabled ? "Cannot change this field." : ""}
 		/>
-		<label className="absolute text-sm text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-left peer-focus:left-0 peer-focus:text-purple-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+		<label
+			htmlFor={props.name}
+			className="absolute text-sm text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-left 
+		peer-focus:left-0 peer-focus:text-purple-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+		>
 			{label}
 		</label>
 	</div>
