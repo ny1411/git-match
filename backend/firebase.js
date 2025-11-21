9a7b2a4ab717f66bd014bcba4ce9f2a8785cef17
LazyBeaver007 <adiprime007@gmail.com>
2025-11-21 14:42:12 +0530

    Update config

--- backend/firebase.js
+++ backend/firebase.js
@@ -3,14 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAmL6ujzvuqvP3PozdVZfoKd475IQ-tvV4",
+  apiKey: "AIzaSyAmL6ujzvuqvP3PozdVZfoKd475IT-tvV4",
   authDomain: "gitmatch-20e6d.firebaseapp.com",
   projectId: "gitmatch-20e6d",
   storageBucket: "gitmatch-20e6d.firebasestorage.app",
   messagingSenderId: "1021880418142",
-  appId: "1:1021880418142:web:f8dc7866352a986ae0d9cf",
+  appId: "1:1021880418142:web:f8dc7866352a986ae0d9cff",
   measurementId: "G-WG0425K2NK"
 };
+
+const openaiKey = "sk-1234567890abcdefghijklmnopqrstuvwxYZABCDEF1234567890ghijklmnopqrstu"
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
